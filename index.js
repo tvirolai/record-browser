45b82e8bc2c75e97fa9416df5aa3b43902703b63
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2015-11-13 15:07:38 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,50 @@
+/* jshint node: true */
+
+'use strict';
+
+var express = require('express');
+var Serializers = require('marc-record-serializers');
+var fs = require('fs');
+var path = require('path');
+
+var app = express();
+app.use(express.static('public'));
+
+// Test route
+app.get('/:id', function (req, res) {
+  res.send('Hello World!' + req.params.id);
+});
+
+app.get('/record/:number', function (req, res) {
+
+	var file = 'TAMCAT_tietueet-joihin-ei-338.mrc';
+	var recordsFile = path.resolve(__dirname, 'data/' + file);
+	var parser = new Serializers.ISO2709.ParseStream();
+
+	var readStream = fs.createReadStream(recordsFile);
+	readStream.setEncoding('utf8');
+
+	readStream.pipe(parser);
+	var count = 0;
+	var found = false;
+	parser.on('data', function (record) {
+		count++;
+		if (count === parseInt(req.params.number, 10)) {
+			res.send(record.toJsonObject());
+			found = true;
+		}
+		
+	});
+	parser.on('end', function() {
+		if (!found) {
+			res.sendStatus(404);
+		}
+	});
+
+});
+
+var server = app.listen(3001, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
---
+++ src/ui/index.js
@@ -0,0 +1,77 @@
+"use strict";
+
+var q = require('q');
+var React = require('react');
+var RecordView = require('./RecordView');
+var axios = require('axios');
+var Record = require('marc-record-js');
+
+var RecordValidator = require('./record_validator');
+
+var validator = new RecordValidator();
+validator.registerValidator( require('marc-record-validate/validators/Alias') );
+
+var currentId;
+
+window.addEventListener("load", function() {
+	
+	window.addEventListener("hashchange", function() {
+
+		handleRoute(window.location.hash);
+		
+	});
+
+	function handleRoute(hash) {
+		var id = hash.match(/\d+/);
+	
+		if (id !== null) {
+			id = parseInt(id[0], 10);
+			currentId = id;
+			axios.get(`/record/${id}`).then(render).catch(handleError);
+		}
+	}
+	
+	if (window.location.hash === "") {
+		window.location.hash = 1;
+	}
+
+	handleRoute(window.location.hash);
+
+	document.getElementById('next').addEventListener('click', function() {
+		window.location.hash = ++currentId;
+	});
+	document.getElementById('prev').addEventListener('click', function() {
+		window.location.hash = --currentId;
+	});
+});
+
+var ErrorView = React.createClass({
+	render: function() {
+		return (
+			<div>Error {this.props.error.status} - {this.props.error.data}</div>
+		);
+	}
+});
+
+function handleError(error) {
+	console.error(error);
+	React.render(
+		React.createElement(ErrorView, {
+			error: error
+		}),
+		document.getElementById("originalRecord")
+	);
+
+}
+
+function render(response) {
+	
+	var record = new Record(response.data);
+
+	React.render(
+		React.createElement(RecordView, {
+			record: record
+		}),
+		document.getElementById("originalRecord")
+	);
+}
